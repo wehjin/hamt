200c58d6b40b4a60a10b9b363c65eb68fecca27e
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-04-16 11:04:42 -0700

    Renames [SegmentedItemStore] to [ItemStore].

--- src/item_store.rs
+++ src/item_store.rs
@@ -8,26 +8,26 @@ mod tests {
 
 	#[test]
 	fn basic() {
-		let mut store = SegmentedItemStore::new();
+		let mut store = ItemStore::new();
 		let item = 42;
 		let item_ref = store.push(item);
 		assert_eq!(&item, item_ref.borrow());
 	}
 }
 
-pub struct SegmentedItemStore<T> {
+pub struct ItemStore<T> {
 	mem_segment: Rc<Segment<T>>,
 }
 
-impl<Item> SegmentedItemStore<Item> {
+impl<Item> ItemStore<Item> {
 	pub fn new() -> Self {
 		Self { mem_segment: Rc::new(Segment::new()) }
 	}
 
-	pub fn push(&mut self, item: Item) -> SegmentItemRef<Item> {
+	pub fn push(&mut self, item: Item) -> ItemRef<Item> {
 		let (segment, offset) = self.mem_segment.clone_add_item(item);
 		self.mem_segment = Rc::new(segment);
-		SegmentItemRef { pos: SegmentItemPos::Mem(offset), segment: self.mem_segment.clone() }
+		ItemRef { pos: ItemPos::Mem(offset), segment: self.mem_segment.clone() }
 	}
 }
 
@@ -60,25 +60,25 @@ impl<Item> Index<usize> for Segment<Item> {
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum SegmentItemPos {
+enum ItemPos {
 	Mem(usize)
 }
 
-impl SegmentItemPos {
+impl ItemPos {
 	pub fn segment_index(&self) -> usize {
 		match self {
-			SegmentItemPos::Mem(index) => *index
+			ItemPos::Mem(index) => *index
 		}
 	}
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct SegmentItemRef<Item> {
-	pub segment: Rc<Segment<Item>>,
-	pub pos: SegmentItemPos,
+pub struct ItemRef<Item> {
+	segment: Rc<Segment<Item>>,
+	pos: ItemPos,
 }
 
-impl<Item> Borrow<Item> for SegmentItemRef<Item> {
+impl<Item> Borrow<Item> for ItemRef<Item> {
 	fn borrow(&self) -> &Item {
 		&self.segment[self.pos.segment_index()]
 	}
