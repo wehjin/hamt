ae4d26eb8558b0fb9c56fe3119e185a9e73eb3b1
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-04-22 20:11:56 -0700

    Adds failing [persist_empty] test.

--- src/kv_store/tests/persistence.rs
+++ src/kv_store/tests/persistence.rs
@@ -1,2 +1,14 @@
+use crate::kv_store::KvForest;
+use crate::kv_store::tests::{prepare_kv_store_test_dir, TestKey};
+
 #[test]
-fn basic() {}
+fn persist_empty() {
+	let path = prepare_kv_store_test_dir("persist-empty");
+	let index = {
+		let mut forest = KvForest::<TestKey, u32>::open_or_create(&path).expect("open or create");
+		forest.new_trie().expect("index")
+	};
+	let forest = KvForest::<TestKey, u32>::open_or_create(&path).expect("open or create");
+	let size = forest[index].size();
+	assert_eq!(0, size);
+}
