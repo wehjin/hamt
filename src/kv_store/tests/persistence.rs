946bdb87a7d0f0f5d3479fbc3bcc134b5dfae169
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-04-24 14:09:45 -0700

    Converts persist_one to persist_thousand.

--- src/kv_store/tests/persistence.rs
+++ src/kv_store/tests/persistence.rs
@@ -14,15 +14,21 @@ fn persist_empty() {
 }
 
 #[test]
-fn persist_one() {
-	let path = prepare_kv_store_test_dir("persist-one");
+fn persist_thousand_internal_71() {
+	let path = prepare_kv_store_test_dir("persist-thousand-interval-71");
+	dbg!(&path);
 	let index = {
 		let mut forest = KvForest::open_or_create(&path).expect("open or create");
-		let index = forest.add_root().expect("add-root");
-		forest.push(index, 3, 3).expect("push")
+		let mut index = forest.add_root().expect("add-root");
+		for i in 0..1000 {
+			index = forest.push(index, i * 71, i + 1).expect("push");
+		}
+		index
 	};
 	let forest = KvForest::open_or_create(&path).expect("open or create");
 	let trie = forest.trie(index).expect("trie at index");
-	assert_eq!(1, trie.size());
-	assert_eq!(Some(3), trie.find(&3).cloned());
+	assert_eq!(1000, trie.size());
+	for i in 0..1000 {
+		assert_eq!(Some(i + 1), trie.find(&(i * 71)).cloned());
+	}
 }
