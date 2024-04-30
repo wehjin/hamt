8118eb616bbdf2dd0616a6519e299b60c23c5d7e
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-04-29 19:39:36 -0700

    Adds attributes parameter to Db::create.

---
+++ src/attribute.rs
@@ -0,0 +1,2 @@
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub struct Attribute(pub &'static str, pub &'static str);
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,22 @@
+pub mod attribute;
 pub mod datom;
 pub mod item_stash;
 pub mod kv_store;
 pub mod trie;
 
+
 pub mod db {
 	use std::{fs, io};
 	use std::path::{Path, PathBuf};
+
+	use crate::attribute::Attribute;
 
 	pub struct Db {
 		path: PathBuf,
 	}
 
 	impl Db {
-		pub fn create(db_dir: impl AsRef<Path>) -> io::Result<()> {
+		pub fn create(db_dir: impl AsRef<Path>, attributes: impl AsRef<[Attribute]>) -> io::Result<()> {
 			fs::create_dir(db_dir.as_ref())?;
 			Ok(())
 		}
@@ -31,14 +35,18 @@ pub mod db {
 		use std::{env, fs};
 		use std::path::PathBuf;
 
+		use crate::attribute::Attribute;
 		use crate::db::Db;
 
 		#[test]
 		fn basic() {
 			let db_dir = dbg!(ready_test_dir("hamt-db-basic").join("db"));
-			Db::create(&db_dir).unwrap();
+			Db::create(
+				&db_dir,
+				&[Attribute("lot", "size")],
+			).unwrap();
 			let db = Db::open(&db_dir).expect("Open succeeds");
-			assert_eq!(&db_dir, db.path())
+			assert_eq!(&db_dir, db.path());
 		}
 
 		fn ready_test_dir(name: &str) -> PathBuf {
